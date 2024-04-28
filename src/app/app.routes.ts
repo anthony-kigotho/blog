d6f12f871c57027b2d7ab122a0ffaa65b186077d
anthony-kigotho <anthonykigotho7@gmail.com>
2024-04-28 09:12:07 +0300

    Added a new route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { BlogListComponent } from './blog-list/blog-list.component';
 import { BlogDetailComponent } from './blog-detail/blog-detail.component';
 
 export const appRoutes: Routes= [
+  { path: '', component: BlogListComponent },
   { path: 'blogs', component: BlogListComponent },
   { path: 'blog/:postId', component: BlogDetailComponent, data: { title: 'Blog Detail' } },  
 ];
